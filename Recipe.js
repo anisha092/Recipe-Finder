8d9f7da2784df5194328f99876866473b8814e69
Anisha <khatrianisha90@gmail.com>
2024-02-07 23:00:33 +0500

    Function Api call

--- Recipe.js
+++ Recipe.js
@@ -1,8 +1,31 @@
 const KeyApi ='1846dbefe6e04288a4b2a810d0dcf6fe';
+const input= document.getElementById('RecipeInput');
 
-fetch(`https://api.spoonacular.com/recipes/random?apiKey=${KeyApi}`)
-.then(response => response.json())
-  .then(data => {
-    console.log(data);
-  })
+
+const DisplayResults=(id)=>{
+    fetch(`https://api.spoonacular.com/recipes/=${id}/information?apiKey=${KeyApi}`)
+    .then(response=>{
+      const RecipeDetail= document.getElementById('RecipeContainer');
+      RecipeDetail.innerHTML=`
+      <h3 id="title">${RecipeDetail.title} </h3>
+      <img src=" alt="${RecipeDetail.title}"${RecipeDetail.image} ">
+      <p> ${RecipeDetail.instructions} </p>
+      `;
+    })  
+    .catch(error=>{
+      console.log("Error fetching Api details ",error);
+    });
+}
+
+
+const fetchApi=()=>{
+  let recipe= input.value;
+  fetch(`https://api.spoonacular.com/recipes/search?query=${recipe}apiKey=${KeyApi}`)
+  .then(response => response.json())
+    .then(data => {
+        
+    })
+
+}
+
  
