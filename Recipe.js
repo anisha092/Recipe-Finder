7af3352aa30c26fb63eee6d4e78cce29a3ce56cc
Anisha <khatrianisha90@gmail.com>
2024-02-07 16:49:20 +0500

    Fetch Spoonacular API

--- Recipe.js
+++ Recipe.js
@@ -1,10 +1,8 @@
+const KeyApi ='1846dbefe6e04288a4b2a810d0dcf6fe';
 
-const ApiKey ='1846dbefe6e04288a4b2a810d0dcf6fe';
-
-fetch(`https://api.spoonacular.com/recipes/random?apiKey=${ApiKey}`)
+fetch(`https://api.spoonacular.com/recipes/random?apiKey=${KeyApi}`)
 .then(response => response.json())
   .then(data => {
     console.log(data);
   })
-  .catch(error => {
-    console.error('Error fetching')});
+ 
