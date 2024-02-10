b8f69ecbd3b9ff5f7634ddc2a764cdc18f5b0e90
Anisha <khatrianisha90@gmail.com>
2024-02-10 13:39:55 +0500

    Featured

--- Recipe.js
+++ Recipe.js
@@ -1,44 +1,56 @@
-const KeyApi ='1846dbefe6e04288a4b2a810d0dcf6fe';
-const input= document.getElementById('RecipeInput');
+const KeyApi = '1846dbefe6e04288a4b2a810d0dcf6fe';
+let recipeInp = document.getElementById('RecipeInput');
 
+const fetchApi = () => {
+  let recipe = recipeInp.value;
 
-const DisplayResults=(id)=>{
-    fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${KeyApi}`)
-    .then(response=> response.json())
-    .then(RecipeDetail=>{
-      const RecipeData= document.getElementById('RecipeContainer');
-      RecipeData.innerHTML=`
-      <h3 id="title">${RecipeDetail.title} </h3>
-      <img src=" alt="${RecipeDetail.title}"${RecipeDetail.image} ">
-      <p> ${RecipeDetail.instructions} </p>
-      `;
-    })
-      
-    
-
-  }
-
-
-const fetchApi=()=>{
-  let recipe= input.value;
-  fetch(`https://api.spoonacular.com/recipes/search?query=${recipe}apiKey=${KeyApi}`)
-  .then(response => response.json())
+  fetch(`https://api.spoonacular.com/recipes/search?query=${recipe}&apiKey=${KeyApi}`)
+    .then(response => response.json())
     .then(data => {
-      if(data.results && data.results.length>0){
-        const IdRecipe=data.results[0].id;
+      if (data.results && data.results.length > 0) {
+        const IdRecipe = data.results[0].id;
         DisplayResults(IdRecipe);
+      } else {
+        console.log("No Recipe Found");
       }
-      else{
-        console.log("No Recipe Found")
-      }
-      
-
-        
     })
-    .catch(error=>{
-      console.log('Error fetching',error);
+    .catch(error => {
+      console.log('Error fetching:', error);
     });
-
 }
 
- 
+const DisplayResults = (id) => {
+  fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${KeyApi}`)
+    .then(response => response.json())
+    .then(RecipeDetail => {
+      const RecipeData = document.getElementById('RecipeContainer');
+      RecipeData.innerHTML = `
+        <h3 id="title">${RecipeDetail.title} </h3>
+        <img src="${RecipeDetail.image}" alt="${RecipeDetail.title}">
+        <p>${RecipeDetail.instructions}</p>
+      `;
+    })
+    .catch(error => {
+      console.log("Error fetching recipe details", error);
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
