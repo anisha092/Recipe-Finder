506a537280501642bed8a8415fc03e06feef404f
Anisha <khatrianisha90@gmail.com>
2024-02-11 01:37:40 +0500

    Display Recipes

--- Recipe.js
+++ Recipe.js
@@ -1,15 +1,17 @@
 const KeyApi = '1846dbefe6e04288a4b2a810d0dcf6fe';
-let recipeInp = document.getElementById('RecipeInput');
+
 
 const fetchApi = () => {
-  let recipe = recipeInp.value;
+  let recipeInp = document.getElementById('RecipeInput');
+  let RecipeContainer=getElementById("RecipeContainer");
+  let recipe=recipeInp.value;
 
   fetch(`https://api.spoonacular.com/recipes/search?query=${recipe}&apiKey=${KeyApi}`)
     .then(response => response.json())
     .then(data => {
       if (data.results && data.results.length > 0) {
-        const IdRecipe = data.results[0].id;
-        DisplayResults(IdRecipe);
+        const relatedRecipes=data.results.slice(1,7);
+        displayRelatedRecipes(relatedRecipes);
       } else {
         console.log("No Recipe Found");
       }
@@ -19,21 +21,24 @@ const fetchApi = () => {
     });
 }
 
-const DisplayResults = (id) => {
-  fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${KeyApi}`)
-    .then(response => response.json())
-    .then(RecipeDetail => {
-      const RecipeData = document.getElementById('RecipeContainer');
-      RecipeData.innerHTML = `
-        <h3 id="title">${RecipeDetail.title} </h3>
-        <img src="${RecipeDetail.image}" alt="${RecipeDetail.title}">
-        <p>${RecipeDetail.instructions}</p>
+const displayRelatedRecipes = (recipes) => {
+      const relatedRecipesContainer = document.getElementById('RecipeContainer');
+      relatedRecipesContainer.innerHTML = `
+      <h3>Related Recipes <h3/>
+      <div class="row">
+      ${recipes.map(recipe=>`
+         <div class=""col-md-4 mb-4> 
+         <div class="card"><div/>
+         <div class="card-body">
+         <h5 class="card-title">${recipe.title}</h5>
+         <button class="btn btn-primary" onclick="displayRecipeDetails(${recipe.id})">View Recipe</button>
+     </div>
+         <div/>
+      `).join('')}
+      <div/>
       `;
-    })
-    .catch(error => {
-      console.log("Error fetching recipe details", error);
-    });
-}
+    }
+    
 
 
 
